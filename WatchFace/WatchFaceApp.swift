516b8de09a3b28bc659b97c89ae5e62da819610b
Kraig Kistler <kraigwkistler@gmail.com>
2023-04-08 11:19:31 -0700

    Initial Commit

---
+++ WatchFace/WatchFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchFaceApp.swift
+//  WatchFace
+//
+//  Created by Kraig Kistler on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
